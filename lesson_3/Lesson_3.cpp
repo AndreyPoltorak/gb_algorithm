9a52b9204eb8bb21f2129da1085c86d29e0bca23
Andrey_Poltorak <79004862861@yandex.ru>
2021-05-15 10:48:25 +0300

    'lesson_3'

---
+++ lesson_3/Lesson_3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите число:\n";
+    int a;
+    cin >> a;
+    int d = a / 2;
+    while (d > 1)
+    {
+        if (a % d == 0)
+            break;
+        d--;
+    }
+
+    if (d <= 1)
+        cout << " Число = " << a << " - простое!" << endl;
+    else
+        cout << " Число = " << a << " не является простым!" << endl;
+    system("pause");
+}
